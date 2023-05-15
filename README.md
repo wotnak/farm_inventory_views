bb96415ce4ddebdbe5834035a39cd9106b3e6ff9
wotnak <wotnak@pm.me>
2023-05-15 16:45:15 +0200

    add quantity field to assets views

---
+++ README.md
@@ -0,0 +1,3 @@
+# farmOS Inventory Views
+
+farmOS addon module that adds inventory quantity field to assets views.
---
+++ src/Plugin/views/field/AssetQuantity.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\farm_inventory_views\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\EntityField;
+
+/**
+ * A field that displays asset quantity.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("asset_quantity")
+ */
+class AssetQuantity extends EntityField {
+
+}
